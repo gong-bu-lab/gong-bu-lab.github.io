f580e91d26941cfd23c67107e762372b273d6957
Wenqi Wang <wenqi@Wenqis-MacBook-Pro.local>
2025-08-28 22:19:24 -0400

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,12 +9,10 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", {
-        rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-    },
-  }),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
